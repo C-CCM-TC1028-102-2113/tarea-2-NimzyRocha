d5b9562b2accc467731eaf990db0ba401c03a83d
NimzyRocha <89158726+NimzyRocha@users.noreply.github.com>
2021-08-24 17:07:50 -0500

    Update exercise.py

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -6,14 +6,14 @@ def main():
             print ("No cumples requisitos") 
         else:
             edad>=18
-            ID=str(input("¿Tienes identificación oficial? (s/n):"))
+            ID=str(input("¿Tienes identificación oficial? (s/n): "))
             if ID =="s":
                 print ("Trámite de licencia concedido")
             if ID =="n":
                 print ("No cumples requisitos") 
             if (ID != "s") and (ID != "n"):
                 print ("Respuesta incorrecta")
-    edad=int(input("Ingresa tu edad:"))
+    edad=int(input("Ingresa tu edad: "))
     mine ()
     pass
 
