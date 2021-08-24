d1e3599e2931894b47d737c6af8937a476aa63a6
NimzyRocha <89158726+NimzyRocha@users.noreply.github.com>
2021-08-24 15:57:36 -0500

    Update exercise.py

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -3,13 +3,13 @@ def main():
     def main():
         if x>y:
             if x>z:
-                print("el número mayor es:", x)
+                print(x)
         if y>x:
             if y>z:
-                print("el número mayor es:", y)
+                print(y)
         if z>x:
             if z>y:
-                print("el número mayor es:", z)
+                print(z)
     
 
     print("Ingresa el primer número:")
